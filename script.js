3df8f327e3d44f1084bbdfb863b3f7b030e11f86
miranda mason <jmas1229@yahoo.com>
2022-10-27 10:32:51 -0400

    LoopsExercise

---
+++ script.js
@@ -0,0 +1,47 @@
+//Loops Exercise
+//1
+for (i=1; 1<=7; i++){
+    console.log(i);
+}
+
+let b = '=' .repeat(25);
+console.log(b);
+
+//2
+for (i=5; i<=25;i +=4){
+    console.log(i);
+}
+
+console.log(b);
+
+//3a
+const wizards = [
+
+    'Harry Potter',
+    'Hermoine Granger',
+    'Ron Weasley'
+    
+];
+
+//3b
+for (friends of wizards){
+    console.log(friends);
+    
+}
+
+console.log(b);
+ 
+
+//4a
+ let = harryPotterMovies = 0;
+ 
+
+ //4b
+
+ while (harryPotterMovies > 8) {
+        harryPotterMovies++; 
+//4c
+console.log(harryPotterMovies);
+}
+
+
